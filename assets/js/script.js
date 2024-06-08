30a82d0aa941cf77e7539e6ae547f2e7a397b3eb
angganesia <angganesia@gmail.com>
2024-06-08 10:59:02 +0700

    update

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const prefersDark = window.matchMedia('(prefers-color-scheme: darkmode)');
-
-toggleDarkTheme(prefersDark.matches);
-
-prefersDark.addListener((mediaQuery) => toggleDarkTheme(mediaQuery.matches));
-
-function toggleDarkTheme(shouldAdd) {
-  document.body.classList.toggle('darkmode', shouldAdd);
-}
