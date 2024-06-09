2748d07b2efa91898a74bb2c1b250915ecb344b0
angganesia <angganesia@gmail.com>
2024-06-10 01:36:34 +0700

    update categories.js

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,14 +1,14 @@
----
----
+-- -
+-- -
 
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+const categories = { { % for category in site.categories % } { % capture category_name % } { { category | first } } { % endcapture % } { { category_name } }: [{ % for post in site.categories[category_name] % } { url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}` }, { % endfor % }], { % endfor % } }
 
-window.onload = function () {
+window.onload = function() {
   document.querySelectorAll(".category").forEach((category) => {
-    category.addEventListener("click", function (e) {
+    category.addEventListener("click", function(e) {
       const posts = categories[e.target.innerText];
       let html = ``
-      posts.forEach(post=>{
+      posts.forEach(post => {
         html += `
         <a class="modal-article" href="${post.url}">
           <h4>${post.title}</h4>
@@ -23,10 +23,27 @@ window.onload = function () {
     });
   });
 
-  document.querySelector("#category-modal-bg").addEventListener("click", function(){
+  document.querySelector("#category-modal-bg").addEventListener("click", function() {
     document.querySelector("#category-modal-title").innerText = "";
     document.querySelector("#category-modal-content").innerHTML = "";
     document.querySelector("#category-modal-bg").classList.toggle("open");
     document.querySelector("#category-modal").classList.toggle("open");
   })
 };
+
+
+/* =======================
+// Scroll Top Button
+======================= */
+$(".top").click(function() {
+  $("html, body")
+    .stop()
+    .animate({ scrollTop: 0 }, "slow", "swing");
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop() > $(window).height()) {
+    $(".top").addClass("is-active");
+  } else {
+    $(".top").removeClass("is-active");
+  }
+});
