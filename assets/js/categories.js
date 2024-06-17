8d35319ea461b9f292e6526bd1aedeff3157f32a
angganesia <angganesia@gmail.com>
2024-06-17 14:27:00 +0700

    update

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,7 +1,7 @@
 ---
 ---
 
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
@@ -12,6 +12,7 @@ window.onload = function () {
         html += `
         <a class="modal-article" href="${post.url}">
           <h4>${post.title}</h4>
+          <small class="modal-article-date">${post.date}</small>
         </a>
         `
       })
